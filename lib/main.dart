b02474dac53d4d9b0d7ecb9a31fdc97630cf5e5b
Saw-YanLinOo <yly80103@gmail.com>
2023-02-17 17:41:32 +0630

    update firebase configration

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:todo_app/firebase_options.dart';
 import 'package:todo_app/todo_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
