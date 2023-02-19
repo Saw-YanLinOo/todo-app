e45479805a2310225a280f16708485cba9c7a39b
Saw-YanLinOo <yly80103@gmail.com>
2023-02-19 12:31:46 +0630

    ✅ craft widget testing

---
+++ integration_test/test_data/test_data.dart
@@ -0,0 +1,7 @@
+const String todoOne = 'Wait up morning';
+const String todoTwo = 'Moring..';
+const String todoThree = 'Let Go';
+const String todoFour = 'Go to school';
+
+const String add = 'Add';
+
--- integration_test/todo_test.dart
+++ integration_test/todo_test.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -6,6 +7,8 @@ import 'package:todo_app/firebase_options.dart';
 import 'package:todo_app/main.dart';
 import 'package:todo_app/network/api_constant.dart';
 import 'package:todo_app/page/todo_page.dart';
+
+import 'test_data/test_data.dart';
 
 void main() async {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
@@ -19,12 +22,29 @@ void main() async {
   );
 
   testWidgets('Todo App Testing', (WidgetTester tester) async {
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(const MyApp());
 
     await Future.delayed(const Duration(seconds: 2));
 
     await tester.pumpAndSettle(const Duration(seconds: 5));
 
     expect(find.byType(ToDoPage), findsOneWidget);
+
+    expect(find.text(todoOne), findsOneWidget);
+    expect(find.text(todoTwo), findsOneWidget);
+    expect(find.text(todoThree), findsOneWidget);
+    expect(find.byType(TextField), findsOneWidget);
+    expect(find.text(add), findsOneWidget);
+
+    await tester.enterText(find.byType(TextField), todoFour);
+    await tester.pumpAndSettle(const Duration(seconds: 5));
+    await tester.testTextInput.receiveAction(TextInputAction.done);
+    await tester.pumpAndSettle(const Duration(seconds: 5));
+
+    await tester.tap(find.text(add));
+    await tester.pumpAndSettle(const Duration(seconds: 5));
+
+    expect(find.text(todoFour), findsOneWidget);
+    await tester.pumpAndSettle(const Duration(seconds: 5));
   });
 }
